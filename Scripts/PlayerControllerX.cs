d04676b08d61e314c6d50d0cde4abcafc1c6c403
Ant0n8 <94899647+Ant0n8@users.noreply.github.com>
2023-11-21 22:23:07 +0100

    Add files via upload

---
+++ Scripts/PlayerControllerX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private float spawnCooldown = 2.0f;
+    private float lastSpawnTime;
+    private bool canSpawn = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - lastSpawnTime >= spawnCooldown)
+        {
+            canSpawn = true;
+        }
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && canSpawn)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+            lastSpawnTime = Time.time;
+            canSpawn = false;
+        }
+    }
+}
